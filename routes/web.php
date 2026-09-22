1881eaaee04dbaf722f95f26bccca0e59c60db6a
chandru <chandrupillaiceramics@gmail.com>
2026-09-22 12:57:12 +0530

    web php updated

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
-Route::get('/', [PageController::class, 'home'])->name('home');
+
+Route::match(['get', 'post'], '/', [PageController::class, 'index'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/services', [PageController::class, 'services'])->name('services');
 Route::get('/shops', [PageController::class, 'shops'])->name('shops');
